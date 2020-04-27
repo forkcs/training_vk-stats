4fa5dab18c17af47ad7d7232c874521658c10372
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-04-28 00:07:03 +0300

    Refactor fetching messages.

--- utils/vkontakte.py
+++ utils/vkontakte.py
@@ -1,11 +1,25 @@
 from typing import List, Dict
+import vk_api
 
 import config
+
+
+def create_api_connection(login: str, password: str):
+    vk_session = vk_api.VkApi(login=login, password=password,
+                              scope='messages', app_id=config.VK_APP_ID)
+    vk_session.auth()
+    vk = vk_session.get_api()
+    return vk
 
 
 def get_messages_count(conn, user_id: int) -> int:
     msg_count = conn.messages.getHistory(user_id=user_id, count=1, version=config.VK_API_VERSION)['count']
     return msg_count
+
+
+def format_raw_message(message: Dict):
+    m = message
+    return {'date': m['date'], 'out': m['out'], 'text': m['text']}
 
 
 def fetch_messages_by_user_id(conn, user_id: int, pbar=None) -> List[Dict]:
@@ -15,15 +29,19 @@ def fetch_messages_by_user_id(conn, user_id: int, pbar=None) -> List[Dict]:
     :param conn: VkApiMethod object
     :param user_id: vk user id
     :return: list of dicts
+
+    TODO: multi-processing or asynchronous fetching
+    TODO: fetch with bufering and save chunks to temp file
     """
 
     messages = []
     msg_count = get_messages_count(conn, user_id)
     if pbar:
         pbar.start()
-    for i in range(0, msg_count + (msg_count % 200), 200):
-        raw_data = conn.messages.getHistory(user_id=user_id, count=200, offset=i, version=config.VK_API_VERSION)
-        messages_part = [{'date': m['date'], 'out': m['out'], 'text': m['text']} for m in raw_data['items']]
+    for i in range(0, msg_count + (msg_count % config.MESSAGES_PER_REQUEST), config.MESSAGES_PER_REQUEST):
+        raw_data = conn.messages.getHistory(user_id=user_id, count=config.MESSAGES_PER_REQUEST, offset=i,
+                                            version=config.VK_API_VERSION)
+        messages_part = [format_raw_message(m) for m in raw_data['items']]
         messages.extend(messages_part)
         if pbar:
             pbar.update(i)
